e26e4987169f4fffb9205444e224a33abb794025
Cadar Maxamed <cadarmaxamed@Cadars-MacBook-Air.local>
2024-02-25 10:02:41 -0800

    added the base required to create an express application

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-const express = require('express')
+//first step: Require Express
+const express = require('express');
+const users = require('./data/users');
+const posts = require('./data/posts');
+
+const app = express();
+const PORT = 3000;
+
+
+//Middleware-------------------------------------------------
+
+
+
+//Routes-----------------------------------------------------
+app.get('/', (req, res) => {
+  res.send('Express Application in the process');
+});
+
+
+
+// Listen to express on a certain port-----------------------
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+
+//comments should go in between them 
