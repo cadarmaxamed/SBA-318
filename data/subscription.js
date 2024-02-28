d44f0625a6797d30c4fa186aad28c5d8eb6f65e1
Cadar Maxamed <cadarmaxamed@Cadars-MacBook-Air.local>
2024-02-28 06:19:16 -0800

    final code for data

--- data/subscription.js
+++ data/subscription.js
@@ -1,23 +1,22 @@
-// const subscription = [
-//     {
-//       "id": 1,
-//       "name": "Basic Membership",
-//       "price": 50,
-//       "duration": "1 month"
-//     },
-//     {
-//       "id": 2,
-//       "name": "Standard Membership",
-//       "price": 75,
-//       "duration": "3 months"
-//     },
-//     {
-//       "id": 3,
-//       "name": "Premium Membership",
-//       "price": 100,
-//       "duration": "6 months"
-//     },
-//   ];
+const subscription = [
+    {
+      "id": 1,
+      "name": "Basic Membership",
+      "price": 50,
+      "duration": "1 year"
+    },
+    {
+      "id": 2,
+      "name": "Standard Membership",
+      "price": 75,
+      "duration": "2 years"
+    },
+    {
+      "id": 3,
+      "name": "Premium Membership",
+      "price": 100,
+      "duration": "4 years"
+    },
+  ];
 
-//   model.exports = subscription;
-  
+  model.exports = subscription;
